49e10fcdb5f37aa1b6c0fcd660a14305366f2544
Abhishek Jawali <jawalia@amazon.com>
2024-06-25 11:46:27 +0530

    initial cdk

--- software/src/main/java/com/productservice/model/Product.java
+++ software/src/main/java/com/productservice/model/Product.java
@@ -7,6 +7,14 @@ import jakarta.persistence.Id;
 @Entity(name = "products")
 public class Product {
 
+	public Product() {
+	}
+
+	public Product(String productName, Integer productPrice) {
+		this.productName = productName;
+		this.productPrice = productPrice;
+	}
+
 	public Product(Integer productId, String productName, Integer productPrice) {
 		this.productId = productId;
 		this.productName = productName;
