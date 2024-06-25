891ef19214a6adb205623f491bc2914e5dd59169
Abhishek Jawali <jawalia@amazon.com>
2024-06-25 11:47:21 +0530

    fixed

--- src/main/java/com/productservice/model/Product.java
+++ src/main/java/com/productservice/model/Product.java
@@ -7,6 +7,9 @@ import jakarta.persistence.Id;
 @Entity(name = "products")
 public class Product {
 
+	public Product() {
+	}
+
 	public Product(Integer productId, String productName, Integer productPrice) {
 		this.productId = productId;
 		this.productName = productName;
