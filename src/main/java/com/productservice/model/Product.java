f701055a022c44f816d8010ac162ab3a9dc2fc83
Abhishek Jawali <jawalia@amazon.com>
2024-06-25 10:47:27 +0530

    upgraded to java 17

--- src/main/java/com/productservice/model/Product.java
+++ src/main/java/com/productservice/model/Product.java
@@ -1,8 +1,8 @@
 package com.productservice.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity(name = "products")
 public class Product {
